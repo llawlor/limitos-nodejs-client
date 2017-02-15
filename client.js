1425327e79674e5eb4be9e8e0c2f9c4e59e7def5
Lee Lawlor <primemod3@gmail.com>
2017-02-14 23:11:45 -0800

    update client.js

--- client.js
+++ client.js
@@ -4,12 +4,11 @@ const WebSocket = require('ws');
 // variable for the websocket, with the websocket server URL
 const ws = new WebSocket('ws://192.168.0.100:3000/cable');
  
+// create the channel JSON
+var channel_json = JSON.stringify({channel: 'DevicesChannel'});
+
 // when the websocket client connects to the server
 ws.on('open', function() {
-
-  // create the channel JSON
-  var channel_json = JSON.stringify({channel: 'DevicesChannel'});
-
   // create the device subscription
   var device_subscription = {
     command: 'subscribe',
@@ -22,16 +21,31 @@ ws.on('open', function() {
  
 // when the websocket receives a message
 ws.on('message', function(data, flags) {
-  // debug: log the full message
-  console.log('message received: ' + data);
-
   // get the JSON data
   var json_data = JSON.parse(data);
  
   // if this is a ping command
   if (json_data.type === 'ping') {
     // log the ping message
-    console.log('ping: ' + json_data.message);
+    //console.log('ping: ' + json_data.message);
+  // else if this is a subscription confirmation
+  } else if (json_data.type === 'confirm_subscription') {
+    console.log('subscribed to: ' + json_data.identifier);
+  // else if this is a device message, handle it
+  } else if (json_data.identifier === channel_json) {
+    handleDeviceMessage(json_data.message);
+  // else log the full message
+  } else {
+    console.log('message received: ' + data);
   }
 
 });
+
+// handle a device message
+function handleDeviceMessage(message) {
+  if (message.my_message === 'on') {
+    console.log('on');
+  } else if (message.my_message === 'off') {
+    console.log('off');
+  }
+}
